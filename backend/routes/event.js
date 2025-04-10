f607bfd1fd52111c85a7e4c64b79ac3dee4b0f6f
Kelly <guojiyou40020@hotmail.com>
2025-04-10 14:19:31 -0400

    fix: add event auth bypass

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -4,12 +4,18 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.get("/readEvent", eventController.getAllEvents);
-router.post("/addEvent", eventController.addEvent);
-router.patch("/updateEvent/:id", 
+router.post(
+  "/addEvent",
+  authController.restrict("Administrator"),
+  eventController.addEvent,
+);
+router.patch(
+  "/updateEvent/:id",
   authController.restrict("Administrator"),
   eventController.updateEvent,
 );
-router.delete("/deleteEvent/:id",
+router.delete(
+  "/deleteEvent/:id",
   authController.restrict("Administrator"),
   eventController.deleteEvent,
 );
@@ -18,6 +24,6 @@ router.post(
   "/upload",
   eventController.upload.single("file"),
   eventController.multerErrorHandling,
-  eventController.handleFileUpload
+  eventController.handleFileUpload,
 ); //For upload images in form event
 module.exports = router;
